0022a7e1bb22554561dae63083958c21456a1b9c
Turkie25 <91883547+Turkie25@users.noreply.github.com>
2023-09-28 12:06:50 +0200

    schoonmaak

--- VanRijtDataBase/VanRijtDataBase/Repositories/UserRepository/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VanRijtDataBase.Repositories.UserRepository
-{
-    public interface IUserRepository
-    {
-        Task<User?> GetUserByIdAsync(int id);
-        Task<List<User>> GetUsersAsync();
-        Task AddUserAsync(User newUser);
-    }
-}
