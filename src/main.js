cc2d59bb8c84d412244cdea76f045273ac526d7d
Turkie25 <91883547+Turkie25@users.noreply.github.com>
2023-09-28 14:11:32 +0200

    login page frontend
    on deze te runnen moet je  npm run start in terminal typen.

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+import Inlog from "./pages/Inlog.vue";
+import "./global.css";
+
+const routes = [
+  {
+    path: "/",
+    name: "Inlog",
+    component: Inlog,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((toRoute, fromRoute, next) => {
+  const documentTitle =
+    toRoute?.meta && toRoute?.meta?.title
+      ? toRoute?.meta?.title
+      : "LoginVanRijt";
+  window.document.title = documentTitle;
+  if (toRoute?.meta?.description) {
+    addMetaTag(toRoute?.meta?.description);
+  }
+  next();
+});
+
+const addMetaTag = (value) => {
+  let element = document.querySelector(`meta[name='description']`);
+
+  if (element) {
+    element.setAttribute("content", value);
+  } else {
+    element = `<meta name="description" content="${value}" />`;
+    document.head.insertAdjacentHTML("beforeend", element);
+  }
+};
+
+const vuetify = createVuetify({ components, directives });
+
+createApp(App).use(router).use(vuetify).mount("#app");
+
+export default router;
---
+++ src/pages/Inlog.vue
@@ -0,0 +1,242 @@
+<template>
+  <div :class="$style.inlog">
+
+    <div :class="$style.frame1">
+      <img :class="$style.frameIcon" alt="" src="/frame.svg" />
+      <div :class="$style.frame2">
+        <div :class="$style.frame3">
+          <a
+            :class="$style.vanRijtAutomatisering"
+            href="http://"
+            target="_blank"
+            >van Rijt automatisering © 2023</a
+          >
+        </div>
+        <div :class="$style.frame4" id="LoginForm">
+          <div :class="$style.loginParent">
+            <div :class="$style.login">Login</div>
+            <div :class="$style.email">Email</div>
+            <div :class="$style.wachtwoord">Wachtwoord</div>
+            <button :class="$style.inloggenWrapper" id="Submit">
+              <div :class="$style.inloggen">Inloggen</div>
+            </button>
+            <v-text-field
+              :class="$style.frameVtextfield"
+              color="primary"
+              variant="outlined"
+              type="email"
+              name="Email"
+            />
+            <div :class="$style.frame5">
+              <button :class="$style.wijzigWachtwoord" id="ChangePassword">
+                wijzig wachtwoord
+              </button>
+              <v-text-field
+                :class="$style.frameVtextfield1"
+                color="primary"
+                variant="outlined"
+                :append-inner-icon="showPasswordGm ? 'mdi-eye' : 'mdi-eye-off'"
+                v-model="passwordGm"
+                :type="showPasswordGm ? 'text' : 'password'"
+                @click:appendInner="togglePasswordVisibilityGm"
+                name="Password"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "Inlog",
+  data() {
+    return { showPasswordGm: false, passwordGm: "" };
+  },
+  methods: {
+    togglePasswordVisibilityGm() {
+      this.showPasswordGm = !this.showPasswordGm;
+    },
+  },
+});
+</script>
+<style module>
+
+.frame {
+  position: absolute;
+  top: 2.88rem;
+  left: 2.81rem;
+  width: 8.94rem;
+  height: 64.06rem;
+  overflow: hidden;
+}
+.frameIcon {
+  position: relative;
+  width: 51rem;
+  height: 70.06rem;
+  overflow: hidden;
+  flex-shrink: 0;
+}
+.vanRijtAutomatisering {
+  position: absolute;
+  top: 67.19rem;
+  left: 0rem;
+  text-decoration: underline;
+  font-weight: 800;
+  color: inherit;
+}
+.frame3 {
+  position: absolute;
+  top: 0rem;
+  left: 24.63rem;
+  width: 14.38rem;
+  height: 68.44rem;
+  overflow: hidden;
+}
+.login {
+  position: absolute;
+  top: 4rem;
+  left: 3rem;
+  font-size: 2.75rem;
+  font-weight: 600;
+}
+.email {
+  position: absolute;
+  top: 10rem;
+  left: 3rem;
+  font-weight: 500;
+}
+.wachtwoord {
+  position: absolute;
+  top: 20rem;
+  left: 3rem;
+  font-weight: 500;
+}
+.inloggen {
+  position: relative;
+  font-size: var(--font-size-7xl);
+  font-weight: 500;
+  font-family: var(--font-inter);
+  color: var(--color-white);
+  text-align: left;
+}
+.inloggenWrapper {
+  cursor: pointer;
+  border: 1px solid rgba(255, 255, 255, 0.8);
+  padding: 0.63rem;
+  background-color: var(--primary);
+  position: absolute;
+  top: 34rem;
+  left: 14rem;
+  border-radius: 16px;
+  box-sizing: border-box;
+  width: 14.81rem;
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: center;
+}
+.frameVtextfield {
+  width: 26rem;
+  border: none;
+  background-color: transparent;
+  position: absolute;
+  top: 13rem;
+  left: 3rem;
+}
+
+.wijzigWachtwoord {
+  cursor: pointer;
+  border: none;
+  padding: 0;
+  background-color: transparent;
+  position: absolute;
+  top: 3.63rem;
+  left: 16rem;
+  font-size: var(--font-size-mini);
+  font-weight: 800;
+  font-family: var(--font-manrope);
+  color: var(--color-darkgray);
+  text-align: center;
+  display: inline-block;
+  width: 9.25rem;
+  height: 1.25rem;
+}
+
+.frameVtextfield1 {
+  width: 26rem;
+  border: none;
+  background-color: transparent;
+  position: absolute;
+  top: 0rem;
+  left: 0rem;
+}
+.frame5 {
+  position: absolute;
+  top: 23rem;
+  left: 3rem;
+  width: 27.41rem;
+  height: 4.88rem;
+  overflow: hidden;
+}
+.loginParent {
+  position: absolute;
+  top: 0.06rem;
+  left: 0rem;
+  border-radius: 56px;
+  background-color: var(--color-white);
+  box-shadow: var(--dropshadow);
+  width: 33.13rem;
+  height: 40rem;
+}
+.frame4 {
+  position: absolute;
+  top: 8rem;
+  left: 5rem;
+  width: 33.13rem;
+  height: 46.06rem;
+  overflow: hidden;
+  font-size: var(--font-size-7xl);
+  color: var(--color-black);
+  font-family: var(--font-inter);
+}
+.frame2 {
+  position: relative;
+  width: 39rem;
+  height: 60.44rem;
+  overflow: hidden;
+  flex-shrink: 0;
+  margin-left: -2.87rem;
+}
+.frame1 {
+  position: absolute;
+  top: -0.12rem;
+  left: -20rem;
+  width: 105rem;
+  height: 50.06rem;
+  overflow: hidden;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  text-align: center;
+  font-size: var(--font-size-mini);
+  color: var(--color-darkgray);
+  font-family: var(--font-manrope);
+}
+.inlog {
+  position: relative;
+  background-color: #eef1f6;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  text-align: left;
+  font-size: 2.81rem;
+  color: #049fb4;
+  font-family: var(--font-inter);
+}
+</style>
