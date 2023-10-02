f4371fa5ab4c34371b1df48f1816be8880915eb4
StanVerbeek12 <91727928+StanVerbeek12@users.noreply.github.com>
2023-10-02 14:02:47 +0200

    Structure edit

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
 <template>
-  <router-view />
+  <div>
+    <LiveDataView/>
+  </div>
 </template>
+
+<script>
+import LiveDataView from "views/LiveDataView"
+
+
+export default {
+  name: 'App',
+  components: {
+    LiveDataView,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body{all: unset;}
+</style>
---
+++ src/views/LiveDataView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <VisitorsCircleCount/>
+  </div>
+</template>
+
+<script>
+import VisitorsCircleCount from '../components/VisitorsCircleCount'
+export default {
+    name: 'LiveDataView',
+  components: {
+    VisitorsCircleCount,
+  }
+}
+</script>
+
+<style>
+
+</style>
