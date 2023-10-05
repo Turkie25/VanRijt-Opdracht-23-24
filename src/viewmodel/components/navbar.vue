2fec3931d441e7e602eb8ec383715f0c99d5277b
amiri888 <amirdessouky@gmail.com>
2023-10-05 09:49:29 +0200

    navbar, weatherblock, profielpicture

---
+++ src/viewmodel/components/navbar.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="navbar">
+      <nav>
+        <ul>
+            <li><router-link to="/"></router-link></li>
+        </ul>
+      </nav>
+    </div>
+  </template>
+  
+
+  <script>
+  export default {
+    name: 'navbar',
+  };
+  
+  </script>
+
+  
+  <style scoped>
+  .navbar {
+    width: 450px;
+    background-color: #FFF;
+    color: #fff;
+    padding: 20px;
+    position: fixed;
+    top: 0;
+    right: 0;
+    height: 100%;
+  }
+  
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  
+  li {
+    margin-bottom: 10px;
+  }
+
+  router-link {
+  color: #fff;
+  text-decoration: none;
+  }
+
+  router-link:hover {
+    
+  color: #f39c12;
+  }
+  </style>
+  
---
+++ src/viewmodel/components/weatherBlock.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="weatherBlock">
+        
+    </div>
+</template>
+
+<style scoped>
+
+.weatherBlock{
+position: absolute;
+width: 396px;
+height: 205px;
+left: 500px; /* verwijder dit */
+background: #515151;
+border-radius: 16px;
+
+}
+
+</style>
