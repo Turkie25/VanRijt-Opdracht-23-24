75433292ecbda6136623e8d674892b63ced4afea
StanVerbeek12 <91727928+StanVerbeek12@users.noreply.github.com>
2023-10-05 09:38:12 +0200

    Squashed commit of the following:
    commit d0c7f6ad32fbeb1adc223d37eb40b6ccf326093e
    Author: Jesper Knez <jesperknez@gmail.com>
    Date:   Thu Oct 5 09:28:59 2023 +0200
    
        bugfix dubbele app.vue
    
    commit 2570885c7dd1de1de6cd6bdea094734fe906ea4a
    Author: Jesper Knez <jesperknez@gmail.com>
    Date:   Thu Oct 5 09:13:23 2023 +0200
    
        Squashed commit of the following:
    
        commit c7a2931d1c8dfc6e0e2d8400bd1c160ec57f2f3e
        Author: StanVerbeek12 <91727928+StanVerbeek12@users.noreply.github.com>
        Date:   Wed Oct 4 09:42:23 2023 +0200
    
            Circular progress bar
    
    commit b1f64edd70fa9d9b32dcec1916b74958427a855c
    Author: Jesper Knez <jesperknez@gmail.com>
    Date:   Thu Oct 5 09:07:48 2023 +0200
    
        Squashed commit of the following:
    
        commit d245b79cddda2198ff624b3606733d0e5ea66835
        Author: Jesper Knez <jesperknez@gmail.com>
        Date:   Thu Oct 5 09:07:17 2023 +0200
    
            root.scss bijgewerkt
    
            Root.scss bijgewerkt. Beschikt nu over alle kleuren en heeft daarnaast wat default styling voor height problemen te corrigeren
    
    commit 23009401cbd65992d0ceceb30118bac3efed52f1
    Author: Turkie25 <91883547+Turkie25@users.noreply.github.com>
    Date:   Wed Oct 4 13:47:57 2023 +0200
    
        login method fixed and working
    
        made the method that handels the login
    
    commit 9dc4228e9dc4a86645bcddcd1a2396b76d32bee7
    Author: Turkie25 <91883547+Turkie25@users.noreply.github.com>
    Date:   Wed Oct 4 13:19:50 2023 +0200
    
        api url
    
    commit 5e9c1f3523cc8a8d9f98008534473aa8b826fed7
    Author: Turkie25 <91883547+Turkie25@users.noreply.github.com>
    Date:   Wed Oct 4 13:14:50 2023 +0200
    
        login method
    
    commit ed8315925570705bb13c2a4247ec3dab6570898d
    Author: Jesper Knez <jesperknez@gmail.com>
    Date:   Tue Oct 3 10:24:06 2023 +0200
    
        Kleuren root.scss
    
        Kleuren als root variabelen toegevoegd in SCSS

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="app">
     <router-view></router-view> <!-- This is where the views will be rendered -->
-  </div>
 </template>
+<style lang="scss">
+@import './view/scss/root.scss';
+</style>
 
 <script>
 export default {
