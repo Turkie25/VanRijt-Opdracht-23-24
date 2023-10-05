66c564cb55c2c6f1a10cccc05706041d8a39fb21
amiri888 <amirdessouky@gmail.com>
2023-10-05 09:15:24 +0200

    clock

---
+++ src/viewmodel/components/realTimeClock.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="realTimeClock">
+        <h2>{{ currentTime }}</h2>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'realTimeClock',
+    data() {
+      return {
+        currentTime: this.getCurrentTime()
+      };
+    },
+    methods: {
+      getCurrentTime() {
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        return `${hours}:${minutes}`;
+      },
+      updateTime() {
+        this.currentTime = this.getCurrentTime();
+      }
+    },
+    mounted() {
+      // update time every minute
+      setInterval(this.updateTime, 60000);
+    }
+  };
+  </script>
+  
+  <style scoped>  
+  h2{
+    color: #04A0B5;
+    font-size: 96px;
+    font-family: 'Manrope';
+  }
+  </style>
+  
