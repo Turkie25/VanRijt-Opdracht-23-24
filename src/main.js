d9b2a8f9e0343b21bbe82c7826599a6698213d70
Turkie25 <91883547+Turkie25@users.noreply.github.com>
2023-10-06 14:01:00 +0200

    .

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
-import Inlog from "./view/pages/Inlog.vue";
 import Login from "./view/pages/Login.vue";
 import "./global.css";
 import router from "../src/router/router";
--- src/view/pages/LiveDataView.vue
+++ src/view/pages/LiveDataView.vue
@@ -17,7 +17,7 @@ export default {
     dataViewCircle,
     dataInformation,
     FestivalBezoekersData,
-    adminPaneluitlogBox,
+    adminPaneluitlogBox
     }
   // ...
 }
