d44c0e105c0a2629db427d824b4f06266085bb28
Turkie25 <91883547+Turkie25@users.noreply.github.com>
2023-10-06 11:44:55 +0200

    LoginPage backend voor database

--- src/view/pages/Login.vue
+++ src/view/pages/Login.vue
@@ -6,11 +6,12 @@
           <h1>Inloggen</h1>
           <div>
             <label for="email">Emailadres</label>
-            <input type="email" placeholder="gebruikersnaam@domein.nl" v-model="email" required>
+            <input type="email" name='Email' placeholder="gebruikersnaam@domein.nl" v-model="email" required>
           </div>
           <div>
             <label for="password">Wachtwoord</label>
-            <input type="password" placeholder="********" v-model="password" required>
+            <input type="password" name="Password" placeholder="********" v-model="passwordGm" required>
+
           </div>
           <button type="submit" @click="sendLoginDataToApi">Inloggen</button>
           <span>Geen inloggevens? <a href="">Meer informatie</a></span>
@@ -24,19 +25,61 @@
 import ImageFrame from "../../viewmodel/components/imageFrame.vue";
 import imageFrame from "../../viewmodel/components/imageFrame.vue";
 import { defineComponent, ref } from "vue";
+import axios from 'axios';
+
 
 export default defineComponent({
   name: "Login",
+
   data() {
+    return {
+      email: '',       // Data property to store the email value
+      passwordGm: '',  // Data property to store the password value
+      showPasswordGm: false,
+
+    //  { showPasswordGm: false, passwordGm: "" }
+    };
+  },
+    data() {
     return { showPasswordGm: false, passwordGm: "" };
   },
   methods: {
-    togglePasswordVisibilityGm() {
-      this.showPasswordGm = !this.showPasswordGm;
+    sendLoginDataToApi() {
+      // Define the API URL
+      const apiUrl = 'https://localhost:7129/api/Login';
+
+      // Define the data to send in the request body using the captured values
+      const requestBody = {
+        EMail: this.email,        // Use the email data property
+        Password: this.passwordGm, // Use the passwordGm data property
+      };
+
+      // Define the headers you want to send with the request
+      const headers = {
+        'Content-Type': 'application/json', // Replace with the appropriate content type
+      };
+
+      // Send a POST request to the API with headers and the request body
+      axios.post(apiUrl, requestBody, { headers })
+        .then((response) => {
+          // Handle the response data here
+          console.log(response.data);
+        })
+        .catch((error) => {
+          // Handle any errors here
+          console.error(error);
+        });
+
+
     },
+             togglePasswordVisibilityGm() {
+          this.showPasswordGm = !this.showPasswordGm;
+          },
   },
+
   components: { ImageFrame }
-});
+})
+
 </script>
 <style lang="scss">
 @import '../scss/root.scss';
