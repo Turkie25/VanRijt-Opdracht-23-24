3d2a82a9bbd2ca5dddf5ca75052cbb8ff12d0e75
Turkie25 <91883547+Turkie25@users.noreply.github.com>
2023-10-06 11:25:28 +0200

    bezoekrs informatie component

--- src/view/pages/LiveDataView.vue
+++ src/view/pages/LiveDataView.vue
@@ -2,16 +2,19 @@
   <div class="LiveDataView">
   <dataViewCircle></dataViewCircle>
   <dataInformation></dataInformation>
+  <FestivalBezoekersData></FestivalBezoekersData>
 </div>
 </template>
 <script>
 import dataViewCircle from "../../viewmodel/components/dataViewCircle.vue"
 import dataInformation from "../../viewmodel/components/dataInformation.vue"
+import FestivalBezoekersData from "../../viewmodel/components/FestivalBezoekersData.vue"
 
 export default {
   components: {
     dataViewCircle,
-    dataInformation
+    dataInformation,
+    FestivalBezoekersData,
     }
   // ...
 }
